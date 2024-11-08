1315a0d566e8edc79877eae52c3df407553bfe6e
Youssef-Abdelouali <youssefabdelouali225@gmail.com>
2024-11-08 15:27:29 +0000

    tasks

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -6,10 +6,19 @@ import datetime
 
 for i in range(10000):
     sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
+    ip = "{}.{}.{}.{}".format(
+        random.randint(1, 255),
+        random.randint(1, 255),
+        random.randint(1, 255),
+        random.randint(1, 255)
+    )
+    timestamp = datetime.datetime.now()
+    status_code = random.choice([200, 301, 400, 401, 403, 404, 405, 500])
+    data_size = random.randint(1, 1024)
+
+    sys.stdout.write(
+        "{} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+            ip, timestamp, status_code, data_size
+        )
+    )
     sys.stdout.flush()

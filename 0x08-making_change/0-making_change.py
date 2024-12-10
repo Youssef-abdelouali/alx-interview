b90cba7f2482b5240b0a4817c5a92a6d4fcfc1a3
Youssef-Abdelouali <youssefabdelouali225@gmail.com>
2024-12-10 12:54:22 -0800

    tasks done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,44 @@
 #!/usr/bin/python3
 """
-A function to determine the fewest number of coins
+Module: Game of choosing Prime numbers 
 """
 
-def makeChange(coins, total):
-    """Given a pile of coins of different values"""
-    
-    # If total is 0 or negative, return 0 as no coins are needed
-    if total <= 0:
-        return 0
 
-    # Initialize a dp array where dp[i] is the minimum number of coins needed to make amount i
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: 0 coins are needed to make amount 0
+def primeNumbers(n):
+    """Return list of prime numbers between 1 and n inclusive
+       Args:
+        n (int): upper boundary of range. lower boundary is always 1
+    """
+    primeNos = []
+    filtered = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (filtered[prime]):
+            primeNos.append(prime)
+            for i in range(prime, n + 1, prime):
+                filtered[i] = False
+    return primeNos
 
-    # Iterate through each coin
-    for coin in coins:
-        # Update the dp array for each possible amount from the coin value to total
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    # If dp[total] is still infinity, it's not possible to make the total with the given coins
-    return dp[total] if dp[total] != float('inf') else -1
+def isWinner(x, nums):
+    """
+    Determines winner of Prime Game
+    Args:
+        x (int): no. of rounds of game
+        nums (int): upper limit of range for each round
+    Return:
+        Name of winner (Maria or Ben) or None if winner cannot be found
+    """
+    if x is None or nums is None or x == 0 or nums == []:
+        return None
+    Maria = Ben = 0
+    for i in range(x):
+        primeNos = primeNumbers(nums[i])
+        if len(primeNos) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
